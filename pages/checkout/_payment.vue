c9e2ecd6b472c156bfd33a96834ebaf9fa1d275c
jilling kingma <jilling@jillings-MacBook-Air.local>
2022-02-18 10:33:26 +0100

    cleanup

--- pages/checkout/_payment.vue
+++ pages/checkout/_payment.vue
@@ -15,12 +15,12 @@ let AdyenCheckout;
 if (process.client) {
   AdyenCheckout = require("@adyen/adyen-web");
 }
-
 // Used to finalize a checkout call in case of redirect
+/*
 const urlParams = new URLSearchParams(window.location.search);
-var sessionId = null /*urlParams.get('sessionId');*/ // Unique identifier for the payment session
-var redirectResult = null /*urlParams.get('redirectResult');*/
-var localclientKey = null
+let sessionId = null; /!*urlParams.get('sessionId');*!/ // Unique identifier for the payment session
+let redirectResult = null; /!*urlParams.get('redirectResult');*!/
+let localclientKey = null;
 
 
 async function startCheckout(ref) {
@@ -48,6 +48,7 @@ async function startCheckout(ref) {
 
 // Some payment methods use redirects. This is where we finalize the operation
 async function finalizeCheckout() {
+  console.log("heyo")
   try {
     // Create AdyenCheckout re-using existing Session
     const checkout = await createAdyenCheckout({id: sessionId});
@@ -135,9 +136,17 @@ function handleServerResponse(res, component) {
     }
   }
 }
+*/
 
 export default {
   components: {},
+  data() {
+    return {
+      sessionId: null,
+      redirectResult: null,
+      localclientKey: null,
+    }
+  },
   head() {
     return {
       title: "Payment page",
@@ -147,11 +156,127 @@ export default {
     return { type: route.params.payment };
   },
   mounted() {
-    if (!sessionId) {
-      startCheckout(this);
+    alert(localStorage.getItem('sessionID'))
+    if (localStorage.getItem('sessionID')) {
+      alert("hey")
+      this.finalizeCheckout();
+      localStorage.clear()
     } else {
-      // existing session: complete Checkout
-      finalizeCheckout();
+      this.startCheckout();
+    }
+  },
+  methods: {
+    async startCheckout() {
+      alert("starting checkout");
+      console.log(this.type);
+      try {
+        // Init Sessions
+        let { response, clientKey } = await this.callServer("/api/sessions?type=" + this.type);
+
+        // Set global variables
+        this.localclientKey = clientKey;
+        const checkoutSessionResponse = response;
+        this.sessionId = checkoutSessionResponse.id;
+        localStorage.setItem('sessionID', this.sessionId);
+        this.redirectResult = checkoutSessionResponse.redirectResult;
+
+        // Create AdyenCheckout using Sessions response
+        const checkout = await this.createAdyenCheckout(checkoutSessionResponse);
+
+        // Create an instance of Drop-in and mount it
+        checkout.create(this.type).mount(this.$refs[this.type]);
+
+      } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details");
+      }
+    },
+
+    async finalizeCheckout() {
+      alert('finalize')
+      try {
+        // Create AdyenCheckout re-using existing Session
+        const checkout = await this.createAdyenCheckout();
+        // Submit the extracted redirectResult (to trigger onPaymentCompleted() handler)
+        checkout.submitDetails({details: this.redirectResult});
+      } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details");
+      }
+    },
+
+    async createAdyenCheckout(session) {
+      const configuration = {
+        clientKey: this.localclientKey,
+        locale: "en_US",
+        environment: "test",
+        showPayButton: true,
+        session: session,
+        paymentMethodsConfiguration: {
+          ideal: {
+            showImage: true
+          },
+          card: {
+            hasHolderName: true,
+            holderNameRequired: true,
+            name: "Credit or debit card",
+            amount: {
+              value: 1000,
+              currency: "EUR"
+            }
+          },
+          paypal: {
+            amount: {
+              currency: "USD",
+              value: 1000
+            },
+            environment: "test",
+            countryCode: "US"   // Only needed for test. This will be automatically retrieved when you are in production.
+          }
+        },
+        onPaymentCompleted: (result, component) => {
+          console.log("result: " + result);
+          this.handleServerResponse(result, component);
+        },
+        onError: (error, component) => {
+          console.error(error.name, error.message, error.stack, component);
+        }
+      };
+
+      return new AdyenCheckout(configuration);
+    },
+
+    async callServer(url, data) {
+      const res = await fetch(url, {
+        method: "POST",
+        body: data ? JSON.stringify(data) : "",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return await res.json();
+    },
+
+    async handleServerResponse(res, component) {
+      if (res.action) {
+        component.handleAction(res.action);
+      } else {
+        switch (res.resultCode) {
+          case "Authorised":
+            window.location.href = "/result/success";
+            break;
+          case "Pending":
+          case "Received":
+            window.location.href = "/result/pending";
+            break;
+          case "Refused":
+            window.location.href = "/result/failed";
+            break;
+          default:
+            window.location.href = "/result/error";
+            break;
+        }
+      }
     }
   },
 };
