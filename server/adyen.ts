b4d4579cbe60c444d5710d1af5a6bf3c5b583715
Beppe Catanese <gcatanese@yahoo.com>
2024-08-27 10:52:31 +0200

    Add try-catch

--- server/adyen.ts
+++ server/adyen.ts
@@ -2,13 +2,17 @@ import adyen from "@adyen/api-library";
 
 export async function createAdyenClient() {
 
-    const { CheckoutAPI, Client, Config } = adyen;
-    
-    var config = new Config();
-    config.apiKey = process.env.ADYEN_API_KEY;
+    try {
+        const { CheckoutAPI, Client, Config } = adyen;
 
-    const client = new Client({ config });
-    client.setEnvironment('TEST'); // Use 'LIVE' for production
+        var config = new Config();
+        config.apiKey = process.env.ADYEN_API_KEY;
 
-    return new CheckoutAPI(client);
+        const client = new Client({ config });
+        client.setEnvironment('TEST'); // Use 'LIVE' for production
+
+        return new CheckoutAPI(client);
+    } catch (error) {
+        throw new Error("Cannot create Adyen client", { cause: error })
+    }
 }
